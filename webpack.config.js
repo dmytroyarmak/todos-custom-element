92c48881ce04b7fa5b7bb4a3fe9ce093f9c505b9
Dmytro Yarmak <dmytroyarmak@gmail.com>
2017-11-21 09:40:29 +0200

    Configure Minify and Bundle Analyzer plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+// const MinifyPlugin = require('babel-minify-webpack-plugin');
+// const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
   entry: './src/index.js',
@@ -8,11 +10,12 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
-  devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist'
   },
   plugins: [
+    // new MinifyPlugin({}, {comments: false}),
+    // new BundleAnalyzerPlugin(),
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
       template: './src/index.html'
